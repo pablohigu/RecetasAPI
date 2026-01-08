f369faedb98cf3cae8aa6c624161e3fa43e8d43e
pablohigu <pablo.hi.do@gmail.com>
2026-01-08 13:29:47 +0100

    chore: Dividir el MasterController en RecipeType y NutientType Controllers

--- src/Controller/NutrientTypeController.php
+++ src/Controller/NutrientTypeController.php
@@ -3,33 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\NutrientType;
-use App\Entity\RecipeType;
 use App\Model\NutrientTypeDTO;
-use App\Model\RecipeTypeDTO;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MasterDataController extends AbstractController
+#[Route('/nutrient-types')]
+class NutrientTypeController extends AbstractController
 {
     public function __construct(private EntityManagerInterface $entityManager) {}
-    #[Route('/recipe-types', name: 'get_recipe_types', methods: ['GET'])]
-    public function getRecipeTypes(): JsonResponse
-    {
-        // 1. Recuperar entidades (PDF pág. 52)
-        $types = $this->entityManager->getRepository(RecipeType::class)->findAll();
-        
-        // 2. Convertir a DTOs
-        $data = [];
-        foreach ($types as $type) {
-            $data[] = new RecipeTypeDTO($type->getId(), $type->getName(), $type->getDescription());
-        }
 
-        return $this->json($data);
-    }
-
-    #[Route('/nutrient-types', name: 'get_nutrient_types', methods: ['GET'])]
+    #[Route('', name: 'get_nutrient_types', methods: ['GET'])]
     public function getNutrientTypes(): JsonResponse
     {
         $types = $this->entityManager->getRepository(NutrientType::class)->findAll();
---
+++ src/Controller/RecipeTypeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\RecipeType;
+use App\Model\RecipeTypeDTO;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/recipe-types')]
+class RecipeTypeController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $entityManager) {}
+
+    #[Route('', name: 'get_recipe_types', methods: ['GET'])]
+    public function getRecipeTypes(): JsonResponse
+    {
+        // 1. Recuperar entidades
+        $types = $this->entityManager->getRepository(RecipeType::class)->findAll();
+        
+        // 2. Convertir a DTOs
+        $data = [];
+        foreach ($types as $type) {
+            $data[] = new RecipeTypeDTO($type->getId(), $type->getName(), $type->getDescription());
+        }
+
+        return $this->json($data);
+    }
+}
